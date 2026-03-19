fe601fa1301945b2ccb4d5041c551cf0d66c5290
huntcode <wanglv666@gmail.com>
2026-03-19 14:39:05 +0800

    1.打印发送的plist数据内容

--- libs/cast/eshare/src/EshareSessionSelfTest.cpp
+++ libs/cast/eshare/src/EshareSessionSelfTest.cpp
@@ -8,12 +8,39 @@
 #include <thread>
 
 #include "EshareSessionClient.h"
+#include "EsharePlistExtract.h"
 
 using namespace WQt::Cast::Eshare;
 
 #ifndef ESHARE_TESTDATA_DIR
 #define ESHARE_TESTDATA_DIR "."
 #endif
+
+static void DumpLoadedPlist(const QString& tag, const QByteArray& data)
+{
+    qDebug().noquote() << "";
+    qDebug().noquote() << "[PLIST] =====" << tag << "=====";
+    qDebug().noquote() << "[PLIST] size =" << data.size();
+
+    if (data.isEmpty())
+    {
+        qDebug().noquote() << "[PLIST] empty";
+        return;
+    }
+
+    QString error;
+    const QString xml = EsharePlistExtract::ToXml(data, &error);
+    if (!xml.isEmpty())
+    {
+        qDebug().noquote() << xml;
+    }
+    else
+    {
+        qDebug().noquote() << "[PLIST] parse failed:" << error;
+        qDebug().noquote() << "[PLIST] HEX:";
+        qDebug().noquote() << data.toHex(' ');
+    }
+}
 
 static QByteArray LoadBinaryFile(const QString& path)
 {
@@ -87,6 +114,9 @@ int main(int argc, char *argv[])
     const QString baseDir = QStringLiteral(ESHARE_TESTDATA_DIR);
     const QByteArray videoBody = LoadBinaryFile(baseDir + "/51040_video_setup_body.bin");
     const QByteArray audioBody = LoadBinaryFile(baseDir + "/51040_audio_setup_body.bin");
+
+    DumpLoadedPlist("51040_video_setup_body.bin", videoBody);
+    DumpLoadedPlist("51040_audio_setup_body.bin", audioBody);
 
     session->Set51040RequestBodies(videoBody, audioBody);
 
